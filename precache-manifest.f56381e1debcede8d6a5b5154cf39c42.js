7cf072e1f83053c7674b894377deab00e7ad5d01
Bill Rogers <bill@Bills-MacBook-Air.local>
2022-07-07 11:51:08 +0300

    Updates

---
+++ precache-manifest.f56381e1debcede8d6a5b5154cf39c42.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d68fd9281ff473fd45f6",
+    "url": "/react-quiz2/static/css/main.d1906552.chunk.css"
+  },
+  {
+    "revision": "d68fd9281ff473fd45f6",
+    "url": "/react-quiz2/static/js/main.a4f63748.chunk.js"
+  },
+  {
+    "revision": "3c61bab5380cc0f10cc8",
+    "url": "/react-quiz2/static/js/runtime~main.fa36d346.js"
+  },
+  {
+    "revision": "abd515914ec14d78765d",
+    "url": "/react-quiz2/static/js/2.70b3c4b4.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/react-quiz2/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "559f166d947d2e7b4e0d4bb426d6632b",
+    "url": "/react-quiz2/static/media/Flag_of_Rwanda.559f166d.svg"
+  },
+  {
+    "revision": "aa572e0e7ad47c23e37633f1b370da8d",
+    "url": "/react-quiz2/static/media/Flag_of_Kenya.aa572e0e.svg"
+  },
+  {
+    "revision": "f8bb736e5832232610b5b65dd3c0a121",
+    "url": "/react-quiz2/static/media/Flag_of_Uganda.f8bb736e.svg"
+  },
+  {
+    "revision": "5ec3b115b6f327284a2414d7ec3f5a9b",
+    "url": "/react-quiz2/static/media/Flag_of_Tanzania.5ec3b115.svg"
+  },
+  {
+    "revision": "08f6719ece3f3a45661e629e8a7a3dc4",
+    "url": "/react-quiz2/static/media/Flag_of_Burundi.08f6719e.svg"
+  },
+  {
+    "revision": "9164da6bef2c499fd1175e86560a79a9",
+    "url": "/react-quiz2/static/media/Flag_of_the_East_African_Community.9164da6b.svg"
+  },
+  {
+    "revision": "51dd25840ef022b279be4f222b42211a",
+    "url": "/react-quiz2/static/media/East_Africa.51dd2584.png"
+  },
+  {
+    "revision": "4604e4bacf9c6e8a359ecf6e5b84a396",
+    "url": "/react-quiz2/static/media/West_Africa.4604e4ba.png"
+  },
+  {
+    "revision": "a470ec8b9dce08ec66b596e384066d7b",
+    "url": "/react-quiz2/static/media/North_Africa.a470ec8b.svg"
+  },
+  {
+    "revision": "df865a2c551b9f46363ed2a25bd11593",
+    "url": "/react-quiz2/static/media/Southern_Africa.df865a2c.png"
+  },
+  {
+    "revision": "49a6f807bcfc3a6e4463a609fd0c084b",
+    "url": "/react-quiz2/static/media/Central_Africa.49a6f807.svg"
+  },
+  {
+    "revision": "f5c0303118c1be62000cc85b6e2a9996",
+    "url": "/react-quiz2/static/media/East_Europe.f5c03031.jpeg"
+  },
+  {
+    "revision": "866960ce3bfe524a3490976df61fe739",
+    "url": "/react-quiz2/static/media/western-europe-countries.866960ce.webp"
+  },
+  {
+    "revision": "cbea8d53f4c70e166e7d0bd812c3a8b4",
+    "url": "/react-quiz2/static/media/North_Europe.cbea8d53.webp"
+  },
+  {
+    "revision": "59a95f722c4ee2d54659a9196c4890a8",
+    "url": "/react-quiz2/static/media/South_Europe.59a95f72.webp"
+  },
+  {
+    "revision": "50802e3a81298a695474c787697ed1f6",
+    "url": "/react-quiz2/static/media/Flag_of_Somalia.50802e3a.svg"
+  },
+  {
+    "revision": "c988475be976f9ef535c144c7f5f4364",
+    "url": "/react-quiz2/index.html"
+  }
+];
